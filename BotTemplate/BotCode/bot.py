098228877142ed7e9d899e6cc03aee7ccae179c9
chloebot1 <chloe.seljak@mail.mcgill.ca>
2025-04-03 17:21:43 -0400

    incorrect auth tocken

--- BotTemplate/BotCode/bot.py
+++ BotTemplate/BotCode/bot.py
@@ -20,8 +20,8 @@ import openai
 import uuid
 
 # sys.stdout.reconfigure(encoding='utf-8')
-openai.api_key= os.getenv('ENV_VAR1')
-
+# openai.api_key= os.getenv('ENV_VAR1')
+openai.api_key= "sk-svcacct-fvUiXhnMpIuoo_H9lm0DqxZZtI1RSaMxV-DzJljHAowgsxhThaAVApAkhaPwc-T3BlbkFJ9TnyT65V6-nkQqcGKqnafZqANFIYPvCC3DZrTn1DM-xbTwKD_c9mI7Ny_3hu0A" 
 class Bot(ABot):
     posts_about_keyword = 0
     bert_model = SentenceTransformer("all-MiniLM-L6-v2")
